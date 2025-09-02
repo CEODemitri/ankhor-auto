826dfe055daf584aa40edf16080324503ad2eecf
ceoDemitri <demichole@gmail.com>
2025-09-02 01:20:45 -0500

    hero section layed. added readme for ref

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ankhor Autos
+
+Car lot based in Bahama, Alabama well known for their inventory of interesting and unique vehichles. Build a website that best tells their story and invites the right customer.
+
+## Tech
+
+HTML, CSS, Javascript, SQL
+
+## To Do
+
+- [ ] Build the Brand from scratch
+- [ ] Landing Page Deployed
+
+Credits
+Home background image:
+https://unsplash.com/@wesleyshen
--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    <section>
+      <header class="hero-section">
+        <nav>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list menu" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+          </svg>
+          <a href="/" class="tooltip-container">Ankhor Autos <span class="tooltip-text">home</span></a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill menu" viewBox="0 0 16 16">
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+          </svg>
+        </nav>
 
-setupCounter(document.querySelector('#counter'))
+        
+        <div>
+          <article>
+            <h1>Rides like lightning. One of Ones.</h1>
+          </article>
+          <button>EXPLORE</button>
+        </div>
+      </header>
+      
+    </section>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
