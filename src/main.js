78f3459d2f39b97991718afcbe0383f234b971f9
ceoDemitri <demichole@gmail.com>
2025-10-03 09:05:14 -0500

    style section update with new images and card style

--- src/main.js
+++ src/main.js
@@ -11,34 +11,45 @@
 
 // setupCounter(document.querySelector("#counter"));
 
-const cars = [
+const carStyles = [
   {
-    name: "2021 Toyota Camry",
-    price: "$22,900",
-    image: "https://source.unsplash.com/featured/?toyota,camry",
+    name: "Chariots of Fire",
+    price: "$52,900",
+    image:
+      "https://images.unsplash.com/photo-1642609630364-a31c1b021771?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    name: "2020 Ford Mustang",
+    name: "Submarines with Wings",
     price: "$26,500",
-    image: "https://source.unsplash.com/featured/?ford,mustang",
+    image:
+      "https://images.unsplash.com/photo-1545289602-6c9479945c49?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHN1Ym1hcmluZXxlbnwwfHwwfHx8MA%3D%3D",
   },
   {
-    name: "2019 Honda Civic",
+    name: "Electric Dreams",
     price: "$17,800",
-    image: "https://source.unsplash.com/featured/?honda,civic",
+    image:
+      "https://images.unsplash.com/photo-1676919296008-d74e423cb44e?q=80&w=1337&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Nimble Navigators",
+    price: "$45,300",
+    image:
+      "https://images.unsplash.com/photo-1601370552761-d129028bd833?q=80&w=2667&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
+// add link
+//
 
 const carGrid = document.querySelector(".car-grid");
 
-cars.forEach((car) => {
+carStyles.forEach((carStyle) => {
   const card = document.createElement("div");
   card.className = "car-card";
   card.innerHTML = `
-    <img src="${car.image}" alt="${car.name}" />
+    <img src="${carStyle.image}" alt="${carStyle.name}" />
     <div class="info">
-      <h3>${car.name}</h3>
-      <p><strong>${car.price}</strong></p>
+      <h3>${carStyle.name}</h3>
+      <p><strong>${carStyle.price}</strong></p>
     </div>
   `;
   carGrid.appendChild(card);
