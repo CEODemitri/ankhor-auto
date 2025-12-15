07d2c1fb17c7fde2a674f4ee8d255f90851deea2
ceoDemitri <demichole@gmail.com>
2025-12-14 23:46:58 -0600

    feat: begins to wire up backend for application

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+🧭 Next Small Steps (When You’re Ready)
+
+Add nodemon
+
+Add .env and config
+
+Add SQLite
+
+Add error-handling middleware
---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import healthRoutes from "./routes/health.routes.js";
+
+const app = express();
+
+// built in middleware
+app.use(express.json());
+app.use("/api/health", healthRoutes);
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+export default app;
